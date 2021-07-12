914ab3695a366b9155f8725f04ccbb307e0f2fe9
WilsonSnowden <wilson15548@gmail.com>
2021-07-12 16:37:59 -0300

    feat: AppService https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('APP_ENV') === 'production') {
-            \URL::forceScheme('https');
+        if(config('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
         }
         //
     }
