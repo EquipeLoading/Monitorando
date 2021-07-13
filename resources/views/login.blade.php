3ce5f8d443fb1ae140ced273801fddf481de57f0
WilsonSnowden <wilson15548@gmail.com>
2021-07-13 16:24:25 -0300

    feat: mobile or pc

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,31 @@
 </head>
 
 <body>
+    <?php
+        $mobile = FALSE;
 
+        $user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
+
+        foreach($user_agents as $user_agent){
+
+            if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
+                $mobile = TRUE;
+
+                $modelo = $user_agent;
+
+                break;
+            }
+        }
+
+        if ($mobile){
+
+            echo "Acesso feito via ".strtolower($modelo);
+
+        }else{
+
+            echo "Acesso feito via computador";
+        }
+    ?>
     <div class="topnav">
         <a class="active" href="#home"> HOME </a>
         <a href="#monitorias"> @lang('lang.Monitorias') </a>
