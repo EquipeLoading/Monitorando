aa098bb5d61b24f1916e1aa148f195094c3d708c
WilsonSnowden <wilson15548@gmail.com>
2021-09-26 13:59:07 -0300

    feat: Forum 90%

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -26,21 +26,20 @@
                 $(document).ready(function() {
                     $(document).on('click', '#adicionarResposta', function(e) {
                         e.preventDefault(); 
-                        $("#novaResposta").append('<form id="formResposta" method="POST" action="{{ route('monitorias.forum', ['id' => $monitoria_id, 'topico' => $topico->id]) }}" enctype="multipart/form-data">' +
+                        $("#novaResposta").append('<form id="formResposta" method="POST" class="column" action="{{ route('monitorias.forum', ['id' => $monitoria_id, 'topico' => $topico->id]) }}" enctype="multipart/form-data">' +
                                                         '@csrf' +
                                                         '<label for="resposta">Resposta</label>' +
                                                         '<textarea type="text" name="resposta">{{ old('resposta') }}</textarea>' + 
-                                                        '<input type="file" class="form-control-file" name="imagem" id="avatarFile" aria-describedby="fileHelp">' +
-                                                        '<small id="fileHelp" class="form-text text-muted"><br/>Insira uma imagem ou um arquivo pdf</small>' +
-                                                        '<button type="submit">Enviar Resposta</button>' +
-                                                        '<button type="button" id="fecharResposta">Fechar</button>' +
+                                                        '<div class="row"><label id="labelAvatar" for="avatarFile"><h5>Enviar foto</h5></label><input type="file" class="form-control-file" name="imagem" id="avatarFile" aria-describedby="fileHelp" buttonText="Your label here.">' +
+                                                        '<button class="button trash cancel marginButton" type="button" id="fecharResposta"><img src="{{ asset("assets/svg/plus.svg") }}" alt="Plus"></button>' +
+                                                        '<button class="button" type="submit"><img src="{{ asset("assets/svg/save.svg") }}" alt="Save"></button></div>' +
                                                     '</form>');
                         $("#adicionarResposta").remove();
                     });
                     $(document).on('click', '#fecharResposta', function(e) {
                         e.preventDefault();
                         $("#formResposta").remove();
-                        $("#novaResposta").append('<button id="adicionarResposta" type="button">Adicionar uma resposta</button>');
+                        $("#novaResposta").append('<button id="adicionarResposta" type="button" class="button">Responder</button>');
                     });
                 });
             </script>
@@ -52,10 +51,6 @@
                         {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
                         {{ $errors->has('resposta') ? $errors->first('resposta') : '' }}
                         {{ $errors->has('imagem') ? $errors->first('imagem') : '' }}
-                        
-                        
-                        <h3>{{ $topico->topico }}</h3>
-                        
                         <div>
                             @foreach($mensagens as $mensagem)
                                 <div id="mensagem{{$mensagem->id}}">
@@ -64,15 +59,18 @@
                                     ?>
                                     @if(isset($exibir) && $exibir[0] == "primeira mensagem")
                                         @foreach($usuarios as $usuario)
-                                            @if($usuario->id == $mensagem->user_id)
-                                                <p>{{ $usuario->nome }}</p>
+                                            @if($usuario->id == $topico->user_id)
+                                                <h3><b>{{ $usuario->nome }}</b><br>{{ $topico->topico }}</h3>
+
                                             @endif
                                         @endforeach
                                         <?php
                                             $mensagemCriadora = $mensagem;
                                         ?>
+
                                         <div id="esquerda">
                                             <h4>{{ $exibir[1] }}</h4>
+
                                             <?php
                                                 $tipoArquivo = null;
                                                 if(isset($mensagem->imagem)){
@@ -121,15 +119,15 @@
                                                 @endif
                                             </div>
                                         </div>
+
                                     @else
                                         @if(isset($mensagemCriadora) && ($mensagemCriadora->user_id == $mensagem->user_id))
-                                            <div id="esquerda">
+                                            <div id="esquerda" >
                                                 @foreach($usuarios as $usuario)
                                                     @if($usuario->id == $mensagem->user_id)
-                                                        <p>{{ $usuario->nome }}</p>
+                                                        <h4 class="border"><b>{{ $usuario->nome }}</b><br>{{ $mensagem->mensagem }}</h4>
                                                     @endif
                                                 @endforeach
-                                                <h4>{{ $mensagem->mensagem }}</h4>
                                                 <?php
                                                     $tipoArquivo = null;
                                                     if(isset($mensagem->imagem)){
@@ -157,7 +155,7 @@
                                                                                                 '<label >Editar texto</label>' +
                                                                                                 '<textarea name="mensagem" form="editarMensagem">{{ $mensagem->mensagem ?? old('mensagem') }}</textarea>' + 
                                                                                                 '<div class="row"><label id="labelAvatar" for="avatarFile"><h5>Enviar foto</h5></label><input type="file" class="form-control-file" name="imagem" id="avatarFile" aria-describedby="fileHelp" buttonText="Your label here.">' +
-                                                                                                '<button id="marginButton" class="button trash" type="submit" name="apagarAnexo"><img src="{{ asset("assets/svg/trash.svg") }}" alt="Trash"></button>' +
+                                                                                                '<button class="button trash marginButton" type="submit" name="apagarAnexo"><img src="{{ asset("assets/svg/trash.svg") }}" alt="Trash"></button>' +
                                                                                                 '<button class="button trash cancel" type="button" id="fecharEdicao{{$mensagem->id}}"><img src="{{ asset("assets/svg/plus.svg") }}" alt="Plus"></button>' +
                                                                                                 '<button class="button" type="submit"><img src="{{ asset("assets/svg/save.svg") }}" alt="Save"></button></div>' +
                                                                                             
@@ -182,10 +180,10 @@
                                             <div id="direita">
                                                 @foreach($usuarios as $usuario)
                                                     @if($usuario->id == $mensagem->user_id)
-                                                        <p>{{ $usuario->nome }}</p>
+                                                        <h4><b>{{ $usuario->nome }}</b><br>{{ $mensagem->mensagem }}</h4>
+
                                                     @endif
                                                 @endforeach
-                                                <h4>{{ $mensagem->mensagem }}</h4>
                                                 <?php
                                                     $tipoArquivo = null;
                                                     if(isset($mensagem->imagem)){
@@ -199,7 +197,7 @@
                                                         <img height="200" width="300" src="{{ $mensagem->imagem }}" />
                                                     @endif
                                                 @endif
-                                                <div class="row">
+                                                <div class="row direita">
                                                     @if($mensagem->user_id == Auth::user()->id)
                                                         <button type="button" id="editarResposta{{$mensagem->id}}" class="button"><img src="{{ asset("assets/svg/edit.svg") }}" alt="Edit"></button>
                                                         <a id="excluirResposta{{$mensagem->id}}" class="button trash" href="{{ route('monitorias.excluir.mensagem', ['id' => $mensagem->id]) }}"><img src="{{ asset("assets/svg/trash.svg") }}" alt="Trash"></a>
@@ -225,7 +223,7 @@
                                                                 $(document).on('click', '#fecharEdicao{{$mensagem->id}}', function(e) {
                                                                     e.preventDefault();
                                                                     $("#editarMensagem").remove();
-                                                                    $("#mensagem{{$mensagem->id}}").append('<div class="row"><button type="button" id="editarResposta{{$mensagem->id}}" class="button"><img src="{{ asset("assets/svg/edit.svg") }}" alt="Edit"></button>' +
+                                                                    $("#mensagem{{$mensagem->id}}").append('<div class="row direita"><button type="button" id="editarResposta{{$mensagem->id}}" class="button"><img src="{{ asset("assets/svg/edit.svg") }}" alt="Edit"></button>' +
                                                                         '<a id="excluirResposta{{$mensagem->id}}" class="button trash" href="{{ route('monitorias.excluir.mensagem', ['id' => $mensagem->id]) }}"><img src="{{ asset("assets/svg/trash.svg") }}" alt="Trash"></a>'
                                                                     );
                                                                 });
@@ -245,6 +243,7 @@
                         </div>
     
                     </div>
+
                     <div id="forum">
                         <h2>Fórum</h2>
 
