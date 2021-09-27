6c193416dfe3557e2ea3ec54bedd12a1fc2725c6
WilsonSnowden <wilson15548@gmail.com>
2021-09-27 14:51:25 -0300

    feat: resetar senha

--- resources/views/definirNovaSenha.blade.php
+++ resources/views/definirNovaSenha.blade.php
@@ -5,26 +5,37 @@
     <meta charset="utf-8" />
     <title> Monitorando - Mudar senha </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/senha.css') }}">
     <link rel="icon" href="{{ asset('assets/png/icon.png') }}">
     <!-- mediaquery -->
 </head>
 
 <body>
-
+    <div id="topBar">
+        <h1>Monitorando</h1>
+    </div>
+    <div class="modalResetSenha">
+        {{ session()->has('status') ? session('status') : '' }}
+        {{ $errors->has('email') ? $errors->first('email') : '' }}
+    </div>
     <form method="POST" action="{{ route('password.update') }}">
+        <h2>Redefina sua senha.</h2>
+        <hr>
         @csrf
         <label for="email">Informe um e-mail</label>
-        <input type="text" name="email" />
+        <input type="text" name="email" placeholder="email@gmail.com"/>
         <p>{{ $errors->has('email') ? $errors->first('email') : '' }}</p>
+        
         <label>Nova senha</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" placeholder="*******"/>
         <p>{{ $errors->has('password') ? $errors->first('password') : '' }}</p>
+
         <label>Insira novamente a senha</label>
-        <input type="password" name="password_confirmation" />
+        <input type="password" name="password_confirmation" placeholder="*******"/>
         <p>{{ $errors->has('password_confirmation') ? $errors->first('password_confirmation') : '' }}</p>
+        
         <input type="hidden" name="token" value="{{ $token }}" />
-        <button type="submit">Enviar</button>
+        <button class="buttonResetar" type="submit"><h3>Enviar</h3></button>
     </form>
 
 </body>
--- resources/views/resetarSenha.blade.php
+++ resources/views/resetarSenha.blade.php
@@ -5,22 +5,44 @@
     <meta charset="utf-8" />
     <title> Monitorando - Mudar senha </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/senha.css') }}">
     <link rel="icon" href="{{ asset('assets/png/icon.png') }}">
     <!-- mediaquery -->
 </head>
 
 <body>
+    <script>
+            $(document).ready(function(){
+                $('#sendSenha').on('click', function() {
+                        $(".modalResetSenha").css('display', 'block');
+                    });
+                });
+    </script>
+    <div id="topBar">
+        <h1>Monitorando</h1>
+    </div>
+
+    <div class="modalResetSenha">
+        aaa
+        {{ session()->has('status') ? session('status') : '' }}
+        {{ $errors->has('email') ? $errors->first('email') : '' }}
+    </div>
 
     <form method="POST" action="{{ route('password.email') }}">
+        <h2>Encontre sua conta.</h2>
+        <hr>
         @csrf
-        <label>Informe um e-mail</label>
-        <input type="text" name="email" />
-        <button type="submit">Enviar</button>
+        <div class="column">
+            <label>Insira seu email para procurar a sua conta.</label>
+            <input type="text" name="email" placeholder="email@gmail.com"/>
+            <div class="row">
+                <a class="buttonResetar" id="buttonVoltar" href="{{ route('login') }}"><h3>Voltar</h3></a>
+                <button id="sendSenha" class="buttonResetar" type="submit"><h3>Enviar</h3></button>
+            </div>
+        </div>
     </form>
 
-    <p>{{ session()->has('status') ? session('status') : '' }}</p>
-    <p>{{ $errors->has('email') ? $errors->first('email') : '' }}</p>
+    
 
 </body>
 
