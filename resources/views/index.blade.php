40a118e09035ff9e59bd897e9424c671fc04da17
Home <equipeloading06@gmail.com>
2021-09-27 16:37:00 -0300

    feat: adição de textos na página index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,8 @@
         <title> Monitorando </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/monitorias.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/textIndex.css') }}">
+        
         <meta charset="utf-8" />
         <link rel="icon" href="{{ asset('assets/png/icon.png') }}">
         <!-- mediaquery -->
@@ -33,6 +35,19 @@
             <?php }?>
 
         </section>  
+
+        <div class="info1">
+            <h2>
+                Monitorando
+            </h2>
+            <p> @lang('lang.Paragrafo1')</p>
+        </div>
+        <div class="info">
+            <h2> @lang('lang.Praticidade') </h2>
+            <p> @lang('lang.Paragrafo2')</p>
+            <h3> @lang('lang.Vergonha') </h3>
+            <p>@lang('lang.Paragrafo3')</p>
+        </div>
     </body>
 
 </html>
